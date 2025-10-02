583d6d3c0f6c007b076dba48aabc0ad349984191
Captune <187126892+Captune@users.noreply.github.com>
2025-10-02 10:03:35 +0900

    small fix

--- sort_by_level.sh
+++ sort_by_level.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail # Fail if code 1
+shopt -s nullglob
 if ! command -v rg >/dev/null 2>&1; then
     echo "Error: \`rg\` (ripgrep) is required but not installed." >&2
     exit 1
@@ -8,9 +9,9 @@ fi
 
 # Root directory containing all version folders
 ROOT_DIR="."
-OUTPUT_DIR="$OUTPUT_DIR/output"
+OUTPUT_DIR="${ROOT_DIR}/output"
 # Output levels greater than it, doesn't affect Utage
-OUTPUT_LEVEL="13"
+OUTPUT_LEVEL="13" # must be integer
 
 # Detect if output dir exists;
 # if exists, mv it to output_bak<unix_time_stamp>
@@ -36,17 +37,15 @@ for version_dir in "${ROOT_DIR}"/*; do
         # Get rounded level, directly
         for diff in 2 3 4 5 6; do
             # removed "2>/dev/null || true", what does it do?
-            leveln=$(rg --pcre2 -o "(?<=^&lv_${diff}=)[0-9.]+" "${maidata_file}")
-            [ -n "${leveln}" ] || continue
+            leveln=$(rg --pcre2 -o "(?<=^&lv_${diff}=)[0-9.]+" "${maidata_file}") || continue
             int=${leveln%.*}
             frac=${leveln#*.}
             case ${frac} in
                 [0-5]) level="${int}" ;;
                 [6-9]) level="${int}+" ;;
-                *) exit 1 ;;
             esac
             
-            if [ "${leveln}" -ge "${OUTPUT_LEVEL}" ]; then
+            if [ "${int}" -ge "${OUTPUT_LEVEL}" ]; then
                 echo -n "${level} " # "level "
                 mkdir -p "${OUTPUT_DIR}/${level}"
                 cp -r "${song_dir}" "${OUTPUT_DIR}/${level}/"
@@ -55,14 +54,14 @@ for version_dir in "${ROOT_DIR}"/*; do
         done
 
         # handle Utage charts (`&lv_7`)
-        levelu=$(rg --pcre2 -o "(?<=^&lv_7=).+" "${maidata_file}")
+        levelu=$(rg --pcre2 -o "(?<=^&lv_7=).+" "${maidata_file}") || true
         if [ -n "${levelu}" ]; then
             echo -n "Utage"
             mkdir -p "${OUTPUT_DIR}/Utage"
             cp -r "${song_dir}" "${OUTPUT_DIR}/Utage/"
             echo -n ". "
+            echo
         fi
-        echo
     done
 done
 
