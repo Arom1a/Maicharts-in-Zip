c509856ac0e06fe37462362b5deb1cf75d050fcc
92 <169070113+I21b@users.noreply.github.com>
2025-10-04 01:11:12 +0900

    Update sort_by_level.sh

--- sort_by_level.sh
+++ sort_by_level.sh
@@ -5,39 +5,30 @@ if ! command -v rg >/dev/null 2>&1; then
     exit 1
 fi
 
-# Root directory containing all version folders
 ROOT_DIR="."
 OUTPUT_DIR="./output"
 
-# Detect if output dir exists;
-# if exists, mv it to output_bak<unix_time_stamp>
 if [ -d "$OUTPUT_DIR" ]; then
     mv "$OUTPUT_DIR" "${OUTPUT_DIR}_bak$(date +%s)"
 fi
 mkdir -p "$OUTPUT_DIR"
 
 for version_dir in "$ROOT_DIR"/*; do
-    # Check if is dir
     [ -d "$version_dir" ] || continue
-    # Check if is output dir
     [ "$(basename "$version_dir")" = "output" ] && continue
 
     for song_dir in "$version_dir"/*; do
-        # Check if is dir
         [ -d "$song_dir" ] || continue
-
         echo "Working on $song_dir"
         maidata_file="$song_dir/maidata.txt"
-        # Check if is file
         [ -f "$maidata_file" ] || continue
 
-        # Extract difficulty 2-6 (what is `&lv_1`???)
+
         for diff in 2 3 4 5 6; do
             level=$(rg --pcre2 -o "(?<=^&lv_${diff}=)[0-9]+" "$maidata_file" 2>/dev/null || true)
             if [ -z "$level" ]; then
                 continue
             fi
-            # Skip if level is less than 12
             if [ "$level" -lt 12 ]; then
                 continue
             fi
@@ -46,8 +37,6 @@ for version_dir in "$ROOT_DIR"/*; do
             #     continue
             # fi
 
-
-            # handling 13+ and 14+
             dest_level="$level"
             if [ "$level" = "13" ] || [ "$level" = "14" ]; then
                 decimal=$(rg --pcre2 -o "(?<=^&lv_${diff}=${level}\.)[0-9]+" "$maidata_file" 2>/dev/null || true)
